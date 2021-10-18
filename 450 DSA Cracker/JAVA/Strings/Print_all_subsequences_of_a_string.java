053968275023cf3364e2cba420fc02228041aa5a
Taranjot Singh <83899629+taranjotsingh23@users.noreply.github.com>
2021-10-18 19:08:41 +0530

    Update Print_all_subsequences_of_a_string.java

--- 450 DSA Cracker/JAVA/Strings/Print_all_subsequences_of_a_string.java
+++ 450 DSA Cracker/JAVA/Strings/Print_all_subsequences_of_a_string.java
@@ -1,1 +1,34 @@
+import java.util.*;
+public class Print_all_subsequences_of_a_string 
+{
+    public static Scanner scn=new Scanner(System.in);
+
+    public static ArrayList<String> gss(String str) 
+    {
+        if(str.length()==0)
+        {
+            ArrayList<String> base=new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        char ch=str.charAt(0);
+        ArrayList<String> recAns=gss(str.substring(1));
+        ArrayList<String> myAns=new ArrayList<>();
+        for(String s:recAns)
+        {
+            myAns.add(s);
+        }
+        for(String s:recAns)
+        {
+            myAns.add(ch+s);
+        }
+        return myAns;
+    }
+
+    public static void main(String[] args)
+    {
+       String str=scn.next();
+       System.out.println(gss(str));
+    }
+}
 
