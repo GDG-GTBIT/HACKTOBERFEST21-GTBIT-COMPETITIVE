545c12f3d6234c25ea51aa6ff24258a50286dd00
Taranjot Singh <83899629+taranjotsingh23@users.noreply.github.com>
2021-10-25 20:47:48 +0530

    Update Largest_rectangular_area_in_histogram.java

--- 450 DSA Cracker/JAVA/Stack/Largest_rectangular_area_in_histogram.java
+++ 450 DSA Cracker/JAVA/Stack/Largest_rectangular_area_in_histogram.java
@@ -1,1 +1,76 @@
+class Solution
+{
+    //Function to find largest rectangular area possible in a given histogram.
+    public static long[] nextSmallerOnLeft(long[] arr,long n)
+  {
+      Stack<Integer> st=new Stack<>();
+      long[] nsl=new long[arr.length];
+      int k=0;
+      for(int i=0;i<arr.length;i++)
+      {
+          while(st.size()>0 && arr[st.peek()]>=arr[i])
+          {
+              st.pop();
+          }
+          if(st.size()==0)
+          {
+              nsl[k]=-1;
+              k++;
+          }
+          else
+          {
+              nsl[k]=st.peek();
+              k++;
+          }
+          st.push(i);
+      }
+      return nsl;
+  }
+  public static long[] nextSmallerOnRight(long[] arr,long n)
+  {
+      Stack<Integer> st=new Stack<>();
+      long[] nsr=new long[arr.length];
+      int k=arr.length-1;
+      for(int i=arr.length-1;i>=0;i--)
+      {
+          while(st.size()>0 && arr[st.peek()]>=arr[i])
+          {
+              st.pop();
+          }
+          if(st.size()==0)
+          {
+              nsr[k]=arr.length;
+              k--;
+          }
+          else
+          {
+              nsr[k]=st.peek();
+              k--;
+          }
+          st.push(i);
+      }
+      return nsr;
+  }
+    public static long getMaxArea(long hist[], long n) 
+    {
+        long[] nsl=nextSmallerOnLeft(hist,n);
+        long[] nsr=nextSmallerOnRight(hist,n);
+        long[] ans=new long[hist.length];
+        int j=0;
+        for(int i=0;i<hist.length;i++)
+        {
+            ans[j]=hist[i]*(nsr[i]-nsl[i]-1);
+            j++;
+        }
+        long maxEle=-(long)1e9;
+        for(int k=0;k<ans.length;k++)
+        {
+            if(ans[k]>maxEle)
+            {
+                maxEle=ans[k];
+            }
+        }
+        return maxEle;
+    }      
+}
 
