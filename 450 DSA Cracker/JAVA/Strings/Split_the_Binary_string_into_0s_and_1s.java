e697225e7ad99f20a4e914d81634a02384b34518
Taranjot Singh <83899629+taranjotsingh23@users.noreply.github.com>
2021-10-25 20:28:09 +0530

    Update Split_the_Binary_string_into_0s_and_1s.java

--- 450 DSA Cracker/JAVA/Strings/Split_the_Binary_string_into_0s_and_1s.java
+++ 450 DSA Cracker/JAVA/Strings/Split_the_Binary_string_into_0s_and_1s.java
@@ -1,1 +1,40 @@
+import java.util.*;
+public class Split_the_Binary_string 
+{
+    public static Scanner scn=new Scanner(System.in);
+
+    public static int split(String str)
+    {
+        int zeroCount=0;
+        int oneCount=0;
+        int ansCount=0;
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)=='0')
+            {
+                zeroCount++;
+            }
+            if(str.charAt(i)=='1')
+            {
+                oneCount++;
+            }
+            if(zeroCount==oneCount)
+            {
+                ansCount++;
+                zeroCount=0;
+                oneCount=0;
+            }
+        }
+        if(ansCount==0)
+        {
+            return -1;
+        }
+        return ansCount;
+    }
+    public static void main(String[] args)
+    {
+        String str=scn.next();
+        System.out.println(split(str));
+    }
+}
 
